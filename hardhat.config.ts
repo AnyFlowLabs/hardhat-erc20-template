b681ce5229b8bdedf60b8b1c7d7fbb1cdc2e3a7d
Yudi Kubota <yudikubota@gmail.com>
2024-10-24 13:38:29 -0300

    add link to docs on hardhat.config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,8 @@ import "@nomicfoundation/hardhat-ignition-viem";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  // With AnyFlow no configuration is needed, just click 'deploy'!
+  // learn more here: https://docs.anyflow.pro/docs/how_it_works/
 };
 
 export default config;
