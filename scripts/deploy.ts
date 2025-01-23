574540d761a1be979d6e09aba57da50771d3f8ad
Yudi Kubota <yudikubota@gmail.com>
2025-01-23 16:42:07 -0300

    feat: add env variables to template project

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,14 +3,15 @@ import hre from "hardhat";
 export async function main() {
     console.log('Deploying AnyFlowERC20...')
 
-    const args = [
-        '0xA71a71A71a71a71A71a71a71a71A71a71A71A71a',   // _initialOwner
-        1 * 10 ** 18,   // _initialSupply
-        'AnyFlowERC20', // _name
-        'AF',           // _symbol
-    ] as const;
+    const args: [string, bigint, string, string] = [
+        process.env.OWNER_ADDRESS || '0xA71a71A71a71a71A71a71a71a71A71a71A71A71a',   // _initialOwner
+        BigInt(process.env.INITIAL_SUPPLY || '100000000000000000000000000'),   // _initialSupply
+        process.env.TOKEN_NAME || 'AnyFlowERC20',  // _name
+        process.env.TOKEN_SYMBOL || 'AF',          // _symbol
+    ];
 
     const contract = await hre.viem.deployContract("AnyFlowERC20", args);
+
 
     console.log(`Contract AnyFlowERC20 deployed to: ${contract.address}`);
 }
